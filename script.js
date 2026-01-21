14ed332fb9c23f062f149a9eb84ac3a10a8d76d0
Yashkamal-dev <saraiyayashkamal@gmail.com>
2026-01-21 19:04:24 +0530

    add delete UI functionality

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let taskInput = document.querySelector(".task-input");
+let addBtn = document.querySelector(".add-btn");
+let tasksCon = document.querySelector(".tasks-con");
+
+let key;
+
+if (localStorage.length < 1) {
+  localStorage.setItem("key", "1");
+  key = 1;
+} else {
+  key = localStorage.getItem("key");
+  key = Number(key);
+}
+
+// event to style filter buttons
+
 let filterBtns = document.querySelectorAll(".btn");
 
 filterBtns.forEach((btn) => {
@@ -7,4 +23,78 @@ filterBtns.forEach((btn) => {
     });
     btn.classList.add("active");
   });
+});
+
+addBtn.addEventListener("click", () => {
+  let task = taskInput.value;
+  taskInput.value = "";
+
+  if (task != "") {
+    let taskob = {
+      key: key,
+
+      task: task,
+
+      completed: false,
+
+      edit: function () {},
+
+      delete: function () {},
+
+      show: function () {
+        let checkTask = document.createElement("div");
+        checkTask.className = "check-task-con";
+
+        let checkInput = document.createElement("input");
+        checkInput.type = "checkbox";
+        checkInput.className = "check";
+
+        let statusImg = document.createElement("img");
+        statusImg.src = "assets/equal.png";
+        statusImg.classList.add("status-img", "pending");
+
+        let para = document.createElement("p");
+        para.className = "task-show";
+        para.textContent = this.task;
+
+        checkTask.append(checkInput, statusImg, para);
+
+        let edtDlt = document.createElement("div");
+        edtDlt.className = "edit-dlt-con";
+
+        let edt = document.createElement("img");
+        edt.src = "assets/pencil.png";
+        edt.className = "pencil-img";
+
+        let dlt = document.createElement("img");
+        dlt.src = "assets/delete.png";
+        dlt.className = "delete-img";
+
+        edtDlt.append(edt, dlt);
+
+        let dv = document.createElement("div");
+        dv.classList.add("ex-task");
+        // dv.innerHTML = taskDetails;
+
+        dv.append(checkTask, edtDlt);
+
+        tasksCon.append(dv);
+      },
+    };
+
+    taskob.show(); // displaying the task on task container after add button
+
+    localStorage.setItem(key, JSON.stringify(taskob));
+
+    key++;
+    localStorage.setItem("key", key);
+  }
+});
+
+tasksCon.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.classList.contains("delete-img")) {
+    let task = e.target.closest(".ex-task");
+    task.remove();
+  }
 });
