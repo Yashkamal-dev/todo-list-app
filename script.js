96896d11b81fbc9cce17f53e6bef6260ffa9c7ed
Yashkamal-dev <saraiyayashkamal@gmail.com>
2026-01-22 14:12:44 +0530

    Fetch tasks from localStorage

--- script.js
+++ script.js
@@ -25,83 +25,97 @@ filterBtns.forEach((btn) => {
   });
 });
 
+function show(funkey, task) {
+  dvKey = funkey;
+
+  let checkTask = document.createElement("div");
+  checkTask.className = "check-task-con";
+
+  let checkInput = document.createElement("input");
+  checkInput.type = "checkbox";
+  checkInput.className = "check";
+
+  let statusImg = document.createElement("img");
+  statusImg.src = "assets/equal.png";
+  statusImg.classList.add("status-img", "pending");
+
+  let para = document.createElement("p");
+  para.className = "task-show";
+  para.textContent = task;
+
+  checkTask.append(checkInput, statusImg, para);
+
+  let edtDlt = document.createElement("div");
+  edtDlt.className = "edit-dlt-con";
+
+  let edt = document.createElement("img");
+  edt.src = "assets/pencil.png";
+  edt.className = "pencil-img";
+
+  let dlt = document.createElement("img");
+  dlt.src = "assets/delete.png";
+  dlt.className = "delete-img";
+
+  edtDlt.append(edt, dlt);
+
+  let dv = document.createElement("div");
+  dv.classList.add("ex-task");
+  dv.id = dvKey;
+  // dv.innerHTML = taskDetails;
+
+  dv.append(checkTask, edtDlt);
+
+  tasksCon.append(dv);
+
+  dlt.addEventListener("click", () => {
+    // id of dv to remove the task from the localstorage
+    let removeId = dv.getAttribute("id");
+    console.log(removeId);
+
+    dv.remove();
+    localStorage.removeItem(removeId);
+  });
+
+  checkInput.addEventListener("change", (e) => {
+    console.log(e);
+
+    if (checkInput.checked) {
+      statusImg.src = "assets/check.png";
+      statusImg.classList.remove("pending");
+      statusImg.classList.add("completed");
+
+      edt.src = "assets/black-check.png";
+      edt.className = "pencil-img";
+      edt.classList.add("completed-edit");
+    } else {
+      statusImg.src = "assets/equal.png";
+      statusImg.classList.remove("completed");
+      statusImg.classList.add("status-img", "pending");
+
+      edt.src = "assets/pencil.png";
+      edt.className = "pencil-img";
+    }
+  });
+}
+
+// to fetch the data from localstorage
+let length = localStorage.length;
+
+for (let i = 1; i < key; i++) {
+  let value = localStorage.getItem(i);
+  let key = i;
+
+  if (value != null) {
+    show(key, value);
+  }
+}
+
 addBtn.addEventListener("click", () => {
   let task = taskInput.value;
   taskInput.value = "";
 
   if (task != "") {
-    function show() {
-      dvKey = key;
-
-      let checkTask = document.createElement("div");
-      checkTask.className = "check-task-con";
-
-      let checkInput = document.createElement("input");
-      checkInput.type = "checkbox";
-      checkInput.className = "check";
-
-      let statusImg = document.createElement("img");
-      statusImg.src = "assets/equal.png";
-      statusImg.classList.add("status-img", "pending");
-
-      let para = document.createElement("p");
-      para.className = "task-show";
-      para.textContent = task;
-
-      checkTask.append(checkInput, statusImg, para);
-
-      let edtDlt = document.createElement("div");
-      edtDlt.className = "edit-dlt-con";
-
-      let edt = document.createElement("img");
-      edt.src = "assets/pencil.png";
-      edt.className = "pencil-img";
-
-      let dlt = document.createElement("img");
-      dlt.src = "assets/delete.png";
-      dlt.className = "delete-img";
-
-      edtDlt.append(edt, dlt);
-
-      let dv = document.createElement("div");
-      dv.classList.add("ex-task");
-      dv.id = dvKey
-      // dv.innerHTML = taskDetails;
-
-      dv.append(checkTask, edtDlt);
-
-      tasksCon.append(dv);
-
-      dlt.addEventListener("click", () => {
-        // id of dv to remove the task from the localstorage
-        let removeId = dv.getAttribute("id");
-        
-        dv.remove();
-        localStorage.removeItem(removeId);
-      });
-
-      checkInput.addEventListener("change", (e) => {
-        console.log(e);
-
-        if (checkInput.checked) {
-          statusImg.src = "assets/check.png";
-          statusImg.classList.remove("pending");
-          statusImg.classList.add("completed");
-
-          edt.src = "assets/black-check.png";
-          edt.className = "pencil-img";
-          edt.classList.add("completed-edit");
-        } else {
-          statusImg.src = "assets/equal.png";
-          statusImg.classList.add("status-img", "pending");
-
-          edt.src = "assets/pencil.png";
-          edt.className = "pencil-img";
-        }
-      });
-    }
-
-    show(); // displaying the task on task container after add button
+    show(key, task); // displaying the task on task container after add button
 
     localStorage.setItem(key, task);
 
